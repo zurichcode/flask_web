220f3f8b8706ec1cbe8114ee825f3f7d5d14e640
zurichcode <58385722+zurichcode@users.noreply.github.com>
2019-12-07 08:35:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_whale():
-    return "good morning keltenstrasse"
+    return "Good morning Fluntern"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
