04fbc30a1bed23e24afbdb02e55e3856306b0626
zurichcode <58385722+zurichcode@users.noreply.github.com>
2019-12-07 08:24:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-# flask_web/app.py
+#!/usr/bin/python
+# coding: utf-8
+from flask import Flask
 
-From flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world:
-    return 'Hey, we have Flask in a Docker container!'
+def hello_whale():
+    return "good morning keltenstrasse"
 
-
-if __name == '__main__':
+if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
